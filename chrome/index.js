97576b6b44bd06add458ddb824a6c0b65ab91695
Ethan Lewis <git AT ethanlew.is>
2025-08-23 23:37:22 -0700

    Revert "Start to make linksUrl configurable"
    This reverts commit 8eebb8dd623cc0cc15aa462523478997bb51d6f1.

--- chrome/index.js
+++ chrome/index.js
@@ -1,16 +1,15 @@
-function cell(text) {
-  const cell = document.createElement("td");
-  const cellText = document.createTextNode(text);
-  cell.appendChild(cellText);
-  return cell;
-}
-
-async function loadTable() {
-  console.debug("loadTable");
+(async () => {
   const links = await getLinks();
 
   const table = document.getElementById("links");
   table.innerHTML = "";
+
+  function cell(text) {
+    const cell = document.createElement("td");
+    const cellText = document.createTextNode(text);
+    cell.appendChild(cellText);
+    return cell;
+  }
 
   Object.entries(links).forEach(([golink, destination]) => {
     const row = document.createElement("tr");
@@ -18,12 +17,4 @@ async function loadTable() {
     row.appendChild(cell(destination));
     table.appendChild(row);
   });
-}
-
-const refresh = document.getElementById("refresh");
-refresh.onclick = async function () {
-  console.debug("refresh onClick");
-  loadTable();
-};
-
-void loadTable();
+})();
--- chrome/links.js
+++ chrome/links.js
@@ -1,16 +1,4 @@
-async function getConfig() {
-  return {
-    linksUrl: "http://localhost:8000/links.json"
-  }
-}
-
-async function setConfig(config) {
-
-}
-
 async function getLinks() {
-  console.debug("getLinks");
-  const config = await getConfig();
-  const linksJson = await fetch(config.linksUrl).then((response) => response.json());
+  const linksJson = await fetch('./links.json').then((response) => response.json());
   return linksJson["links"];
 }
